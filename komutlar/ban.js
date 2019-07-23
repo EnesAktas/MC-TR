152f5b1868dd27e82fc130e81442421a702f808c
Enes Aktas <49610247+EnesAktas@users.noreply.github.com>
2019-07-23 14:29:57 +0300

    Update ban.js

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -7,17 +7,17 @@ exports.run = (client, message, args) => {
   .setColor(0xFF0000)
   .setTimestamp()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`ban` adlı komutu özel mesajlarda kullanamazsın.')
+  .addField('[MC-TR] :warning: Uyarı :warning:', '`ban` adlı komutu özel mesajlarda kullanamazsın.')
   return message.author.sendEmbed(ozelmesajuyari); }
   let guild = message.guild
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
   let modlog = guild.channels.find('name', 'mod-log');
-  if (!modlog) return message.reply('`mod-log` kanalını bulamıyorum.');
-  if (reason.length < 1) return message.reply('Ban sebebini yazmalısın.');
-  if (message.mentions.users.size < 1) return message.reply('Kimi banlayacağını yazmalısın.').catch(console.error);
+  if (!modlog) return message.reply('[MC-TR] `mod-log` adlı kanalını bulamıyorum.');
+  if (reason.length < 1) return message.reply('[MC-TR] Ban sebebini yazmalısın.');
+  if (message.mentions.users.size < 1) return message.reply('[MC-TR] Kimi banlayacağını yazmalısın.').catch(console.error);
 
-  if (!message.guild.member(user).bannable) return message.reply('Yetkilileri banlayamam.');
+  if (!message.guild.member(user).bannable) return message.reply('Yetkilileri banlamaya yetkim bulunmamakta.');
   message.guild.ban(user, 2);
 
   const embed = new Discord.RichEmbed()
